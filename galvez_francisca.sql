2fa3458559fa181295adc94e551f71438cff938f
ProfeFran89 <fgalvezlynch@gmail.com>
2023-08-22 22:03:41 -0400

    Se carga el proyecto completo

---
+++ galvez_francisca.sql
@@ -0,0 +1,13 @@
+-- Database: galvez_francisca
+
+-- DROP DATABASE IF EXISTS galvez_francisca;
+
+CREATE DATABASE galvez_francisca
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
